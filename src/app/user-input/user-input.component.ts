bda977e62f4adecb688209a1725ad25f799a9922
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-21 22:55:01 +0300

    render user-input component

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+  imports: [],
+})
+export class UserInputComponent {}
