bfc14de2ce6cebc44c441c497ea16deb7bf9309b
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-23 16:26:45 +0300

    add currency pipe

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
 })
 export class InvestmentResultsComponent {
   @Input() results?: {
