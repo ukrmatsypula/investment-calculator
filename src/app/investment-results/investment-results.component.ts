9746baaccc5e67112e800ee58b0a15bb60bb1364
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-23 22:46:16 +0300

    add module structure instead of standAlone components

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,13 +1,10 @@
 import { Component, inject, computed } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
-  standalone: true,
-  imports: [CurrencyPipe],
 })
 export class InvestmentResultsComponent {
   private investmentService = inject(InvestmentService);
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,13 +1,10 @@
-import { Component, Output, EventEmitter, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
-  imports: [FormsModule],
 })
 export class UserInputComponent {
   enteredInitiallInvestment = signal('0');
